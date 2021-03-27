d04439df0874981b28bbf68bfeb49013462f793c
reatoretch <reatoretch@gmail.com>
2021-03-27 22:45:44 +0900

    draw field

---
+++ GUI.py
@@ -0,0 +1,35 @@
+import wx
+from Main import Osero
+class GUI(wx.Frame):
+    def __init__(self, parent=None, id=-1, title=None):
+        wx.Frame.__init__(self, parent, id, title)
+        self.panel = wx.Panel(self, size=(300, 200))
+        self.panel.SetBackgroundColour('WHITE')
+        self.panel.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Fit()
+        self.osero=Osero()
+ 
+    def OnPaint(self, event):
+        dc = wx.PaintDC(self.panel)
+        dc.SetPen(wx.Pen('black'))
+        for i in range(self.osero.game.H):
+            for j in range(self.osero.game.W):
+                if self.osero.game.Field[i][j]==0:
+                    dc.SetBrush(wx.Brush("black"))
+                    dc.DrawCircle(50+20*j+10,20+20*i+10,10)
+                elif self.osero.game.Field[i][j]==1:
+                    dc.SetBrush(wx.Brush("white"))
+                    dc.DrawCircle(50+20*j+10,20+20*i+10,10)
+        for i in range(9):
+            y = 20 + 20*i
+            dc.DrawLine(50, y, 210, y)
+        for i in range(9):
+            x = 50 + 20*i
+            dc.DrawLine(x, 20, x, 180)
+ 
+if __name__ == '__main__':
+    app = wx.PySimpleApp()
+    w = GUI(title='wxgr-line')
+    w.Center()
+    w.Show()
+    app.MainLoop()
--- Main.py
+++ Main.py
@@ -3,31 +3,33 @@ import sys
 import time
 from Game import Game
 from Player import Player,AI
-def main():
-    game=Game()
-    players=[AI(hand=0),AI(hand=1)]
-    turn=0
-    while not game.isEnd():
+class Osero():
+    def __init__(self,players=[AI(hand=0),AI(hand=1)]):
+        self.players=players
+        self.game=Game()
+        self.turn=0
+    def run(self):
         availables=[{},{}]
-        for i in range(game.H):
-            for j in range(game.W):
-                availables[0].update(game.isPutOK(j,i,0))
-                availables[1].update(game.isPutOK(j,i,1))
+        for i in range(self.game.H):
+            for j in range(self.game.W):
+                availables[0].update(self.game.isPutOK(j,i,0))
+                availables[1].update(self.game.isPutOK(j,i,1))
         if sum(availables[0].values())==sum(availables[1].values())==0:
-            game.result()
+            self.game.result()
             exit()
-        game.show()
+        self.game.show()
         while True:
-            if sum(availables[turn].values())==0:break
-            x,y=players[turn].nextHand(turn,game.Field,availables[turn])
-            if availables[turn][f"[{x},{y}]"]:
-                game.update(x,y,turn)
+            if sum(availables[self.turn].values())==0:break
+            x,y=self.players[self.turn].nextHand(self.turn,self.game.Field,availables[self.turn])
+            if availables[self.turn][f"[{x},{y}]"]:
+                self.game.update(x,y,self.turn)
                 break
             else:
                 #print("don't put that.",file=sys.stderr)
                 continue
         #time.sleep(1)
-        turn=(turn+1)%2
+        self.turn=(self.turn+1)%2
 
 if __name__=="__main__":
-    main()
+    osero=Osero()
+    osero.run()

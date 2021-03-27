d927e15c35c167f42909949ab5154ccb2b575ab5
reatoretch <reatoretch@gmail.com>
2021-03-27 23:21:34 +0900

    add GUI Player

--- Player.py
+++ Player.py
@@ -12,3 +12,20 @@ class AI():
     def nextHand(self,turn,field,available):
         x,y=random.randint(0,len(field)-1),random.randint(0,len(field)-1)
         return x,y
+
+import time
+class GUIPlayer():
+    def __init__(self,hand):
+        self.hand=hand
+        self.x=-1
+        self.y=-1
+    def setXY(self,x,y):
+        self.x=x
+        self.y=y
+    def nextHand(self,turn,field,available):
+        self.x=-1
+        self.y=-1
+        while True:
+            time.sleep(.1)
+            if self.x==self.y==-1:continue
+            if available[f"[{self.x},{self.y}]"]:return self.x,self.y
